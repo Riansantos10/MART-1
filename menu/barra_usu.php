857d70c9207deb247f18ebe54931dda4c7231917
Riansantos10 <51839619+Riansantos10@users.noreply.github.com>
2021-01-12 11:51:21 -0300

    Update barra_usu.php

--- menu/barra_usu.php
+++ menu/barra_usu.php
@@ -186,7 +186,7 @@
 
     </div>
 
-    <div class = "modal fade bd-example-modal-lg" id = "edi" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class = "modal fade bd-example-modal-lg" id = "editais" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <!-- Modal Header -->
