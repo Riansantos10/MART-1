7e8a1c189e1dbff867df1df2804eb23f4d213acd
Riansantos10 <51839619+Riansantos10@users.noreply.github.com>
2021-01-12 00:44:07 -0300

    Update conexao.php

--- empresa/eventos/conexao.php
+++ empresa/eventos/conexao.php
@@ -1,30 +1,31 @@
-<?php
-
+<?php 
+	
 	//Conectar com o software de banco de dados
 	//MySQL
-
-	$conexao = mysqli_connect("localhost", "root", "");
-
+	
+	/*$conexao = mysqli_connect("localhost", "root", "");
 	if(!$conexao)
 	{
-		print "<h1>Erro_aqui</h1>";
-
+		print "<h1>Erro</h1>";
 		print mysqli_connect_error($conexao). "<br>";
 		exit();
 	}
-
-
+	
+	
 	//selecionando a base de dados
 	$db = mysqli_select_db($conexao, "mart_banco_new");
-
+	
 	if(!$db)
 	{
 		print "a";
 		print "Erro";
 		print mysqli_error($db)."<br>";
 		exit();
-	}
+	}*/
 
+	$conexao = @mysqli_connect("db4free.net:3306", "jarvis_ifsp", "rn123456789", "marteven")
+			or die("Erro ao abrir");
+			
+	mysqli_set_charset($conexao, "utf8");
 	
-
 ?>
