e617ff7d26de6f91885577414cc5a8ab249e206e
Riansantos10 <51839619+Riansantos10@users.noreply.github.com>
2021-01-12 10:48:38 -0300

    Update barra.php

--- inicial/barra.php
+++ inicial/barra.php
@@ -66,32 +66,36 @@
                 
                 <div class = "collapse navbar-collapse" id = "collapse_target">
                      <ul class="navbar-nav justify-content-end">
-                            <li class="nav-item" id = "login">
-                              <a class="font-weight-bold btn btn-light btn btn-outline-dark nav-link" style = "margin-left: 0%;" id = "click_login" data-target="#modalPoll-1" data-toggle="modal"> <button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Login</button></a>
-                            </li>
-                            <div>
-                                <?php include ("../inicial/modal_form.php")?>
-                            </div>
-                            <li class="nav-item" id = "comum">
-                                <a id = "click_comum" class="font-weight-bold btn btn-light btn btn-outline-dark nav-link" style = "margin-left: 0%; text-decoration: none;" href = "../comum/cadastro.php"><button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Realizar cadastro comum</button></a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link font-weight-bold btn btn-light btn btn-outline-dark" id = "click_empre" style = "text-decoration: none; margin-left: 0%;" href = "../empresa/form_empre.php"><button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Realizar cadastro empresarial</button></a>
-                            </li>
-                        </a>
-                        <a href = "pagina_inicial.php" style = "margin-left: 15%;">
-                            <img src = "../logoti.png" style = "height: 50px; margin-left: 10%; "/>
-                        </a>
+                        <li class="nav-item" id = "login">
+                          <a class="font-weight-bold btn btn-light btn btn-outline-dark nav-link" style = "margin-left: 0%;" id = "click_login" data-target="#modalPoll-1" data-toggle="modal"> <button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Login</button></a>
+                        </li>
+                        <div>
+                            <?php include ("../inicial/modal_form.php")?>
+                        </div>
+                        <li class="nav-item" id = "comum">
+                            <a id = "click_comum" class="font-weight-bold btn btn-light btn btn-outline-dark nav-link" style = "margin-left: 0%; text-decoration: none;" href = "../comum/cadastro.php"><button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Realizar cadastro comum</button></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link font-weight-bold btn btn-light btn btn-outline-dark" id = "click_empre" style = "text-decoration: none; margin-left: 0%;" href = "../empresa/form_empre.php"><button style = "background-color: white; border: none; color: black; padding: 4px 4px; text-align: center; text-decoration: none; display: inline-block; font-size: 14px;">Realizar cadastro empresarial</button></a>
+                        </li>
+                        
+                        <li class = "nav-item">
+                            <a href = "pagina_inicial.php" style = "margin-left: 15%;" class = "nav-link">
+                                <img src = "../logoti.png" style = "height: 50px; margin-left: 10%; "/>
+                            </a>
+                        </li>
                       <!--barra de pesquisa-->
-                        <form class="form-inline my-2 my-lg-0" style = "margin-left: 20%;">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar" id = "barra" list = "pes" style = "width: 450px; display: inline-block; border-bottom: 8px solid black;">
+                        <li class = "nav-item"> 
+                            <form class="form-inline my-2 my-lg-0" style = "margin-left: 20%;">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar" id = "barra" list = "pes" style = "width: 450px; display: inline-block; border-bottom: 8px solid black;">
 
-                            <datalist id = "pes">
+                                <datalist id = "pes">
 
-                            </datalist>
-                        </form>
-                     </div>
-                </ul>
+                                </datalist>
+                            </form>
+                         </li>
+                    </ul>
+                </div>
             </nav>
             
          </div>
